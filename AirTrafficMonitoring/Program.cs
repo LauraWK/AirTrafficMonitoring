26dee9b1a49f8581d53da27c6ec9bc48f4318513
LauraWK <wilkenschildt10@gmail.com>
2018-04-05 10:37:23 +0200

    Add project files.

---
+++ AirTrafficMonitoring/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TransponderReceiver;
+
+namespace AirTrafficMonitoring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Reciver = TransponderReceiverFactory.CreateTransponderDataReceiver();
+
+            Reciver.TransponderDataReady += Reciver_TransponderDataReady; //+= betyder man hækler en event på en eventhandler
+
+            Console.ReadKey();
+        }
+        // Event
+        private static void Reciver_TransponderDataReady(object sender, RawTransponderDataEventArgs e) // sender og e er argumnter
+        {
+            var myList = e.TransponderData;
+
+            for (int i = 0; i < myList.Count; i++)
+            {
+                Console.WriteLine(myList[i]);
+            }
+        }
+    }
+}
---
+++ AirTrafficMonitoring/Track.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirTrafficMonitoring
+{
+    class Track
+    {
+        public string Tag { get; set; }
+        public int XCoordinate { get; set; }
+        public int YCoordinate { get; set; }
+        public int Altitude { get; set; }
+        public DateTime Timestamp { get; set; }
+        
+    }
+}
