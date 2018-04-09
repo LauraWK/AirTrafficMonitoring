4ce4b086592422eb9f7494dbad3e5c8f1c30138e
Michelle Wedfald <michelle.wedfald.nielsen@gmail.com>
2018-04-09 14:57:37 +0200

    controllertest

--- Unit.Test.ATM/ControllerTest.cs
+++ Unit.Test.ATM/ControllerTest.cs
@@ -10,16 +10,34 @@ namespace Unit.Test.ATM
     public class ControllerTest
     {
         private ITransponderReceiver receiver;
-        private ITrack track;
-        private ITransponderReceiver uut;
+        private ControllerReceivedData uut;
+      
 
         [SetUp]
         public void Setup()
         {
-            receiver = Substitute.For<ITransponderReceiver>();
-            track = Substitute.For<ITrack>();
-            
+            receiver = Substitute.For<ITransponderReceiver>();        
+            uut = new ControllerReceivedData(receiver);
         }
+
+        [Test]
+        public void StartReceivingData_DataReady_CorrectCall()
+        {
+            uut.StartReceiving();
+            receiver.Received().TransponderDataReady += uut.DataReady;
+        }
+
+        [Test]
+        public void DataReady_CreateTrackIsCalled_PrintToConsole()
+        {
+            var listofdata = new List<string>() { "ATR423;39045;12932;14000;20180412123244765" };
+            uut.DataReady(this, RawTransponderDataEventArgs(listofdata));
+            Assert.AreEqual("Tag: ATR423 XCoordinate: 39045 YCoordinate: 12932 Altitude: 14000 Timestamp: 12-04-2018 12:32:44", Console.Out);
+
+
+        }
+
+
 
         
     }
