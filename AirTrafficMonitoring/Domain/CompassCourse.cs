f00f1e8c1fb39b734fbd0cd40031342f7be86c1b
LauraWK <wilkenschildt10@gmail.com>
2018-04-17 10:15:59 +0200

    Compass

--- AirTrafficMonitoring/Domain/CompassCourse.cs
+++ AirTrafficMonitoring/Domain/CompassCourse.cs
@@ -8,6 +8,40 @@ namespace AirTrafficMonitoring
 {
     public class CompassCourse
     {
+        public int North { get; set; }
+        public int East { get; set; }
+        public int South { get; set; }
+        public int West { get; set; }
+        public int NorthEast { get; set; }
+        public int SouthEast { get; set; }
+        public int SouthWest { get; set; }
+        public int NorthWest { get; set; }
+
+        public int Undefined { get; set; }
+
+        public void CompassCourse1()
+        {
+            North = 0;
+            East = 2;
+            South = 4;
+            West = 6;
+            NorthEast = 1;
+            SouthEast = 3;
+            SouthWest = 5;
+            NorthWest = 7;
+            Undefined = -1;
+        }
+
+        public int Direction(ITrack track1,ITrack track2)
+        {
+            double angle = Math.Atan2(track2.YCoordinate - track1.YCoordinate, track2.XCoordinate - track1.XCoordinate);
+            angle += Math.PI;
+            angle /= Math.PI / 4;
+            int halfQuarter = Convert.ToInt32(angle);
+            halfQuarter %= 8;
+
+            return halfQuarter;
+        }
 
     }
 }
