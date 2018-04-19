b90dad6e9af7efcc88b31e6cf2c32a64b7b1d8a8
Carina Kær <carinakaer@gmail.com>
2018-04-19 10:48:43 +0200

    XML og observer

---
+++ AirTrafficMonitoring/Boundary/SubjectMonitoredPlanes.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AirTrafficMonitoring.Interfaces;
+
+namespace AirTrafficMonitoring.Boundary
+{
+    public abstract class SubjectMonitoredPlanes
+    {
+        private List<IMonitoredPlanes> observerList = new List<IMonitoredPlanes>();
+
+        public void Attach(IMonitoredPlanes observer)
+        {
+            observerList.Add(observer);
+        }
+
+        public void Detach(IMonitoredPlanes observer)
+        {
+            observerList.Remove(observer);
+        }
+
+        public void Notify(string alarm)
+        {
+            foreach (var observer in observerList)
+            {
+                observer.Update(alarm);
+            }
+        }
+    }
+}
