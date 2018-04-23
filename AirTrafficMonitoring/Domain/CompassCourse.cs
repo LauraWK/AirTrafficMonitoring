dfad00f8107943418e499626a72139dbbe0245f3
LauraWK <wilkenschildt10@gmail.com>
2018-04-23 12:57:52 +0200

    Grader

--- AirTrafficMonitoring/Domain/CompassCourse.cs
+++ AirTrafficMonitoring/Domain/CompassCourse.cs
@@ -12,7 +12,7 @@ namespace AirTrafficMonitoring
         public double Direction(ITrack track)
         {
             //ved nord lig 0 er længden til kanten lig 90.000 - det ses som en vektor (0,90000) som vi beregner vinkel med
-            double angleInRadians = Math.Atan2(track.YCoordinate, track.XCoordinate) - Math.Atan2(90000, 0);
+            double angleInRadians = Math.Atan2(track.YCoordinate - 0, track.XCoordinate - 90000);
 
             double angleInDegrees = Math.Abs(angleInRadians / (180 / Math.PI));
 
