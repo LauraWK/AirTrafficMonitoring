c82415605276fb946e493dfc3dd153e4f4421875
RegitzeHess <regitzehess@gmail.com>
2018-04-23 16:08:05 +0200

    Airspace UnitTest

--- AirTrafficMonitoring/Interfaces/IAirspace.cs
+++ AirTrafficMonitoring/Interfaces/IAirspace.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace AirTrafficMonitoring.Interfaces
 {
-    interface IAirspace
+    public interface IAirspace
     {
         bool DefineAirspace(ITrack track);
     }
