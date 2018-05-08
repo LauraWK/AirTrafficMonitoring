c77f9a7ce9301dc4b3129b6afaaa3cf928a86e92
LauraWK <wilkenschildt10@gmail.com>
2018-05-08 09:26:43 +0200

    exe fil til dll

---
+++ Integration.Test.ATM/Tests/SortingPlanesControllerIntegration.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AirTrafficMonitoring;
+using AirTrafficMonitoring.Boundary;
+using AirTrafficMonitoring.Controller;
+using AirTrafficMonitoring.Domain;
+using AirTrafficMonitoring.Interfaces;
+using NSubstitute;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+using TransponderReceiver;
+
+namespace Integration.Test.ATM.Tests
+{
+    [TestFixture()]
+    class SortingPlanesControllerIntegration
+    {
+        private TracksInAirspaceController _TIAC;
+        private ICalculator _calculator;
+        private IMonitoredPlanes _monitoredPlanes;
+        private ISeperationEvent _seperationEvent;
+        private IAirspace _airspace;
+        private SortingPlanesController _sortingPlanesController;
+        private ITrack track1;
+        private ITrack track2;
+        private List<ITrack> _list;
+        private List<ITrack> _list2;
+        private List<ITrack> _list3;
+        private IDisplay _display;
+
+
+        [SetUp]
+        public void SetUp()
+        {
+            _calculator = Substitute.For<ICalculator>();
+            _monitoredPlanes = Substitute.For<IMonitoredPlanes>();
+            _seperationEvent = Substitute.For<ISeperationEvent>();
+            _display = Substitute.For<IDisplay>();
+            _airspace = new Airspace();
+            track1 = new Track()
+            {
+                Tag = "ABC123",
+                XCoordinate = 10000,
+                YCoordinate = 90000,
+                Altitude = 10000,
+                CompassCourse = 180
+                
+            };
+            track2 = new Track()
+            {
+                Tag = "ABC123",
+                XCoordinate = 10000,
+                YCoordinate = 90000,
+                Altitude = 10000,
+                CompassCourse = 180
+            };
+            _list = new List<ITrack>();
+            _list2 = new List<ITrack>();
+            _list3 = new List<ITrack>();
+
+            _sortingPlanesController = new SortingPlanesController(_list,_display,_monitoredPlanes,_list2,_calculator,_list3);
+            _TIAC = new TracksInAirspaceController(_airspace,_sortingPlanesController);
+        }
+
+        [Test]
+        public void CalculateCompassCourse()
+        {
+            _TIAC.AddToList(track1);
+            _TIAC.AddToList(track2);
+            _sortingPlanesController.MatchTracks(track1);
+            _sortingPlanesController.MatchTracks(track2);
+            _calculator.Received().Direction(track1,track2);
+        }
+        [Test]
+        public void CalculateVelocity()
+        {
+            _TIAC.AddToList(track1);
+            _TIAC.AddToList(track2);
+            _sortingPlanesController.MatchTracks(track1);
+            _sortingPlanesController.MatchTracks(track2);
+            _calculator.Received().DetermineVelocity(track1, track2);
+        }
+
+    }
+}
--- Integration.Test.ATM/Tests/TrackFactoryIntegration.cs
+++ Integration.Test.ATM/Tests/TrackFactoryIntegration.cs
@@ -18,46 +18,22 @@ namespace Integration.Test.ATM
     {
         private ReceivedDataController _driver;
         private ITrack _track;
-        private TrackFactory _trackFactory;
         private ITransponderReceiver _receiver;
         private ITracksInAirSpaceController _tracksInAirspace;
-        private IAirspace _airspace;
-        private ISortingPlanesController _sortingPlanes;
-        private IDisplay _display;
-        private List<string> list;
 
         [SetUp]
         public void SetUp()
         {
             _tracksInAirspace = Substitute.For<ITracksInAirSpaceController>();
             _track = Substitute.For<ITrack>();
-            _trackFactory = new TrackFactory();
-            _receiver = Substitute.For<ITransponderReceiver>();
-            _airspace = Substitute.For<IAirspace>();
-            _sortingPlanes = Substitute.For<ISortingPlanesController>();
-            _display = Substitute.For<IDisplay>();
-
             _driver = new ReceivedDataController(_receiver,_tracksInAirspace);
-
-      
         }
 
         [Test]
-        public void Tag_Is_Converted_Correctly()
+        public void Track_Is_Converted_Correctly()
         {
-            var track = "ABC123;30000;40000;10000;20180404121230000";
-            list = new List<string>{track};
-            _driver.DataReady(this, new RawTransponderDataEventArgs(list));
+            _driver.DataReady(this, new RawTransponderDataEventArgs(new List<string>{ "ABC123;30000;40000;10000;20180404221230123" }));
             _tracksInAirspace.Received().AddToList(Arg.Is<ITrack>(s => s.Tag == "ABC123"));
-
-
-            // _driver.DataReady(this, new RawTransponderDataEventArgs(new List<string>{ "ABC123;30000,40000;10000;20180404221230123" }));
-
-
-
-
-            //_display.Received().ShowTrack(Arg.Is<Track>((x) => x.Tag == "ABC123"));
-
         }
     }
 }
